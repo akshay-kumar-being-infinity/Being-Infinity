6163847e97401290a6dc8631fb08fd372b3b0a54
vishal-being-infinity <vishal@being-infinity.com>
2025-12-16 19:50:00 +0530

    Basic Backend Folder Structure Setup

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, This is the First API!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Run Backend
+
+To run the backend:
+
+```bash
+cd backend
+npm install
+npm run dev
